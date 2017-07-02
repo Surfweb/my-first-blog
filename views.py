6554b80c7c92fb0d179cae2bf5216c0406d39248
Surfweb <shdword@yandex.ru>
2017-07-02 17:57:44 +0300

    Add view && template

--- views.py
+++ views.py
@@ -1,7 +1,11 @@
-
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    # Получаем список всех записей в Post отсортированных по дате публикации
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
