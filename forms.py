5e1ca13b83761d5da60c10ea2433ef4260a92d81
Surfweb <shdword@yandex.ru>
2017-07-03 17:12:35 +0300

    add style and form new post

---
+++ forms.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm): # PostForm - name to we form
+
+    class Meta:
+        model = Post 	# set model, defening to create form 
+        fields = ('title', 'text',) 	# set row, defening field form
--- views.py
+++ views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -9,7 +10,22 @@ def post_list(request):
     # Получаем список всех записей в Post отсортированных по дате публикации
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
-
+	
+# предсталение для конкртного поста
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+	
+# предсталение для формы
+def post_new(request):
+        if request.method == "POST":
+            form = PostForm(request.POST) 	# Получаем данные из формы request.POST
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.published_date = timezone.now()
+                post.save()
+                return redirect('post_detail', pk=post.pk)
+        else:
+            form = PostForm()
+        return render(request, 'blog/post_edit.html', {'form': form})
