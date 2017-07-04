71d96e0721133390d28a0b49764927fcc64edaf5
Surfweb <shdword@yandex.ru>
2017-07-04 17:18:57 +0300

    + authorisation system

--- urls.py
+++ urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
 	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'), # detail info for post
 	url(r'^post/new/$', views.post_new, name='post_new'),	# Add new post
 	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),  # url для редактирования формы
+	url(r'^register/$', views.RegisterFormView.as_view(), name='register_user'), # Регистрация
+	url(r'^login/$', views.LoginFormView.as_view(), name='login'), # Вход
+	url(r'^logout/$', views.LogoutView.as_view(), name='logout'), # Выход
 ]
--- views.py
+++ views.py
@@ -1,9 +1,26 @@
 # -*- coding: utf-8 -*-
+
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.shortcuts import redirect
+
+#Регистрация пользователя
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
+
+# спасибо django за готовую форму аутентификации.
+from django.contrib.auth.forms import AuthenticationForm
+# Функция для установки сессионного ключа.
+# По нему django будет определять, выполнил ли вход пользователь.
+from django.contrib.auth import login
+
+# Выход
+from django.http import HttpResponseRedirect
+from django.views.generic.base import View
+from django.contrib.auth import logout
+
 
 # Create your views here.
 
@@ -45,3 +62,49 @@ def post_edit(request, pk):
         else:
             form = PostForm(instance=post)
         return render(request, 'blog/post_edit.html', {'form': form})
+        
+        
+#Регистрация пользователя
+class RegisterFormView(FormView):
+    form_class = UserCreationForm
+
+    # Ссылка, на которую будет перенаправляться пользователь в случае успешной регистрации.
+    # В данном случае указана ссылка на страницу входа для зарегистрированных пользователей.
+    success_url = "/login/"
+
+    # Шаблон, который будет использоваться при отображении представления.
+    template_name = "blog/register.html"
+
+    def form_valid(self, form):
+        # Создаём пользователя, если данные в форму были введены корректно.
+        form.save()
+
+        # Вызываем метод базового класса
+        return super(RegisterFormView, self).form_valid(form)
+        
+# Авторизация
+class LoginFormView(FormView):
+    form_class = AuthenticationForm
+
+    # Аналогично регистрации, только используем шаблон аутентификации.
+    template_name = "blog/login.html"
+
+    # В случае успеха перенаправим на главную.
+    success_url = "/"
+
+    def form_valid(self, form):
+        # Получаем объект пользователя на основе введённых в форму данных.
+        self.user = form.get_user()
+
+        # Выполняем аутентификацию пользователя.
+        login(self.request, self.user)
+        return super(LoginFormView, self).form_valid(form)
+        
+# Выход пользователя
+class LogoutView(View):
+    def get(self, request):
+        # Выполняем выход для пользователя, запросившего данное представление.
+        logout(request)
+
+        # После чего, перенаправляем пользователя на главную страницу.
+        return HttpResponseRedirect("/")
